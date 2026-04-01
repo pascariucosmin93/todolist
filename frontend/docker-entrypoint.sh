131bc174c3efd31fa64eda7984f395c01bb7f0a0
Cosmin <pascariucosmin93@gmail.com>
2026-04-02 01:19:20 +0300

    try fix frontend

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -4,3 +4,7 @@ set -eu
 envsubst '${VITE_API_URL}' \
   < /usr/share/nginx/html/config.template.js \
   > /usr/share/nginx/html/config.js
+
+envsubst '${BACKEND_UPSTREAM_URL}' \
+  < /etc/nginx/templates/default.conf.template \
+  > /etc/nginx/conf.d/default.conf
